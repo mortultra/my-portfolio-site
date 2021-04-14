4ea6e4766572d681173b17a7865716d772ed3e94
andrewcampbell <awr.campbell@gmail.com>
2021-04-14 19:13:57 -0400

    header nav now accessible with tabbing, added project descriptions to design page

--- main.js
+++ main.js
@@ -1,13 +1,39 @@
 // nav menu smooth scroll to section
-const menuOption = document.querySelector('.mainMenu');
+// const menuOption = document.querySelector('.mainMenu');
 
-menuOption.addEventListener('click', (event) => {
-  if (event.target.tagName == 'LI') {
-    const type = event.target.className;
-    const scrollTo = document.querySelector('#' + type);
-    scrollTo.scrollIntoView({ behavior: "smooth" });
-  }
-});
+// menuOption.addEventListener('click', (event) => {
+//   if (event.target.tagName == 'LI') {
+//     const type = event.target.className;
+//     const scrollTo = document.querySelector('#' + type);
+//     scrollTo.scrollIntoView({ behavior: "smooth" });
+//   }
+// });
+
+function anchorLinkHandler(e) {
+    const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+
+    e.preventDefault();
+    const targetID = this.getAttribute("href");
+    const targetAnchor = document.querySelector(targetID);
+    if (!targetAnchor) return;
+    const originalTop = distanceToTop(targetAnchor);
+
+    window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+
+    const checkIfDone = setInterval(function() {
+        const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+        if (distanceToTop(targetAnchor) === 0 || atBottom) {
+            targetAnchor.tabIndex = "-1";
+            targetAnchor.focus();
+            window.history.pushState("", "", targetID);
+            clearInterval(checkIfDone);
+        }
+    }, 100);
+}
+
+const linksToAnchors = document.querySelectorAll('a[href^="#"]');
+
+linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
 
 // nav bar shrink on scroll
 const navScroll = document.getElementById('navScroll');
