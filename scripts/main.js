4b4b17f968367d585a742eb25be38101ffc931cb
andrewcampbell <awr.campbell@gmail.com>
2024-02-06 17:12:49 -0500

    update(animation): add gsap logic

--- scripts/main.js
+++ scripts/main.js
@@ -1,107 +1,107 @@
-import { gsap } from "gsap";
+import { gsap } from "../node_modules/gsap/all.js";
 
-//------------------------------------------------
-// Nav Menu Scroll-Into-View
-//------------------------------------------------
+// //------------------------------------------------
+// // Nav Menu Scroll-Into-View
+// //------------------------------------------------
 
-function anchorLinkHandler(e) {
-  const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
+// function anchorLinkHandler(e) {
+//   const distanceToTop = (el) => Math.floor(el.getBoundingClientRect().top);
 
-  e.preventDefault();
-  const targetID = this.getAttribute("href");
-  const targetAnchor = document.querySelector(targetID);
-  if (!targetAnchor) return;
-  const originalTop = distanceToTop(targetAnchor);
+//   e.preventDefault();
+//   const targetID = this.getAttribute("href");
+//   const targetAnchor = document.querySelector(targetID);
+//   if (!targetAnchor) return;
+//   const originalTop = distanceToTop(targetAnchor);
 
-  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+//   window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
 
-  const checkIfDone = setInterval(() => {
-    const atBottom =
-      window.innerHeight + window.scrollY >= document.body.offsetHeight - 2;
-    if (distanceToTop(targetAnchor) === 0 || atBottom) {
-      targetAnchor.tabIndex = "-1";
-      targetAnchor.focus();
-      window.history.pushState("", "", targetID);
-      clearInterval(checkIfDone);
-    }
-  }, 100);
-}
+//   const checkIfDone = setInterval(() => {
+//     const atBottom =
+//       window.innerHeight + window.scrollY >= document.body.offsetHeight - 2;
+//     if (distanceToTop(targetAnchor) === 0 || atBottom) {
+//       targetAnchor.tabIndex = "-1";
+//       targetAnchor.focus();
+//       window.history.pushState("", "", targetID);
+//       clearInterval(checkIfDone);
+//     }
+//   }, 100);
+// }
 
-const linksToAnchors = document.querySelectorAll('a[href^="#"]');
+// const linksToAnchors = document.querySelectorAll('a[href^="#"]');
 
-linksToAnchors.forEach((each) => (each.onclick = anchorLinkHandler));
+// linksToAnchors.forEach((each) => (each.onclick = anchorLinkHandler));
 
-// accessible scroll into view from https://codepen.io/a8t/pen/JOYwLM?editors=1010
+// // accessible scroll into view from https://codepen.io/a8t/pen/JOYwLM?editors=1010
 
-//------------------------------------------------
-// Nav & Background Box Scroll Behaviour
-//------------------------------------------------
+// //------------------------------------------------
+// // Nav & Background Box Scroll Behaviour
+// //------------------------------------------------
 
-// nav bar shrink on scroll
-const navScroll = document.getElementById("navScroll");
-const logoBox = document.getElementById("logoContainer");
-const topBlock = document.getElementById("topBlock");
-const devBlock = document.getElementById("devBlock");
-const bottomBlock = document.getElementById("bottomBlock");
+// // nav bar shrink on scroll
+// const navScroll = document.getElementById("navScroll");
+// const logoBox = document.getElementById("logoContainer");
+// const topBlock = document.getElementById("topBlock");
+// const devBlock = document.getElementById("devBlock");
+// const bottomBlock = document.getElementById("bottomBlock");
 
-window.onscroll = () => {
-  scrollFunction(), topBoxMovement(), devBoxMovement(), bottomBoxMovement();
-};
+// window.onscroll = () => {
+//   scrollFunction(), topBoxMovement(), devBoxMovement(), bottomBoxMovement();
+// };
 
-scrollFunction = () => {
-  if (document.body.clientWidth < 450) {
-    navScroll.style.padding = "20px 0 35px 0";
-    logoBox.style.width = "50%";
-  } else if (document.body.clientWidth < 768) {
-    navScroll.style.padding = "20px 0 20px 0";
-    logoBox.style.width = "40%";
-  } else if (
-    document.body.scrollTop > 80 ||
-    document.documentElement.scrollTop > 80
-  ) {
-    navScroll.style.padding = "20px 0 15px 0";
-    logoBox.style.width = "25%";
-  } else if (document.body.clientWidth > 768) {
-    navScroll.style.padding = "50px 0 20px 0";
-    logoBox.style.width = "30%";
-  }
-};
+// scrollFunction = () => {
+//   if (document.body.clientWidth < 450) {
+//     navScroll.style.padding = "20px 0 35px 0";
+//     logoBox.style.width = "50%";
+//   } else if (document.body.clientWidth < 768) {
+//     navScroll.style.padding = "20px 0 20px 0";
+//     logoBox.style.width = "40%";
+//   } else if (
+//     document.body.scrollTop > 80 ||
+//     document.documentElement.scrollTop > 80
+//   ) {
+//     navScroll.style.padding = "20px 0 15px 0";
+//     logoBox.style.width = "25%";
+//   } else if (document.body.clientWidth > 768) {
+//     navScroll.style.padding = "50px 0 20px 0";
+//     logoBox.style.width = "30%";
+//   }
+// };
 
-// the element behind the header description moves on scroll of 200px from top
-topBoxMovement = () => {
-  if (
-    document.body.scrollTop > 200 ||
-    document.documentElement.scrollTop > 200
-  ) {
-    topBlock.style.left = "90%";
-  } else {
-    topBlock.style.left = "23%";
-  }
-};
+// // the element behind the header description moves on scroll of 200px from top
+// topBoxMovement = () => {
+//   if (
+//     document.body.scrollTop > 200 ||
+//     document.documentElement.scrollTop > 200
+//   ) {
+//     topBlock.style.left = "90%";
+//   } else {
+//     topBlock.style.left = "23%";
+//   }
+// };
 
-// the background element behind development moves after 1250px of scrolling from top
-devBoxMovement = () => {
-  if (
-    document.body.scrollTop > 1250 ||
-    document.documentElement.scrollTop > 1250
-  ) {
-    devBlock.style.right = "30%";
-  } else {
-    devBlock.style.right = "0%";
-  }
-};
+// // the background element behind development moves after 1250px of scrolling from top
+// devBoxMovement = () => {
+//   if (
+//     document.body.scrollTop > 1250 ||
+//     document.documentElement.scrollTop > 1250
+//   ) {
+//     devBlock.style.right = "30%";
+//   } else {
+//     devBlock.style.right = "0%";
+//   }
+// };
 
-// the background element behind design moves after 2000px of scrolling from top
-bottomBoxMovement = () => {
-  if (
-    document.body.scrollTop > 2000 ||
-    document.documentElement.scrollTop > 2000
-  ) {
-    bottomBlock.style.left = "75%";
-  } else {
-    bottomBlock.style.left = "30%";
-  }
-};
+// // the background element behind design moves after 2000px of scrolling from top
+// bottomBoxMovement = () => {
+//   if (
+//     document.body.scrollTop > 2000 ||
+//     document.documentElement.scrollTop > 2000
+//   ) {
+//     bottomBlock.style.left = "75%";
+//   } else {
+//     bottomBlock.style.left = "30%";
+//   }
+// };
 
 //------------------------------------------------
 // Intro Description Updater
@@ -159,8 +159,21 @@ const interval = setInterval(updateDesc, 1750);
 // GSAP Portfolio Animations
 //------------------------------------------------
 
-
-
 const tl = gsap.timeline();
 
-tl.to(".portfolioCardInfo", {duration: 1.5, top: 0, width: "100%", height: "100%", });
+const portfolioAnimation = tl.from(".portfolioCardInfo", { duration: 0.5, bottom: "102.5%", ease:"bounce.inOut" });
+tl.to(".portfolioCardInfo", { duration: 0.75, bottom: 0, ease: "back.inOut" });
+tl.to(".portfolioCardInfo", { duration: 0.5, height: "100%", width: "100%", ease: "power4.in" });
+
+let hover = false;
+
+const portfolioArray = document.querySelectorAll(".portfolioCard");
+
+portfolioArray.forEach((item) => {
+  item.addEventListener("mouseover", hoverOn);
+
+  function hoverOn() {
+    
+  }
+})
+
