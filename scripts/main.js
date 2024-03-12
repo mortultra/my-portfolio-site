58b68ccdfe8fc0a4215ef6bcdede382a583fc970
andrewcampbell <awr.campbell@gmail.com>
2024-03-12 15:13:40 -0400

    fix(main.js): gsap import syntax

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,6 @@
-import { gsap } from "../node_modules/gsap/all.js";
+import gsap from "../node_modules/gsap/all.js";
+
+// import gsap from "https://cdn.skypack.dev/gsap";
 
 //------------------------------------------------
 // Nav Menu Scroll-Into-View
